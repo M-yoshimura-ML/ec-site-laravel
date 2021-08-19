078fe9b5bd07cca2e85a445c6bd1407861b81593
M-yoshimura-ML <masahiro.yoshimura@freemight.com>
2021-08-19 21:03:47 +0800

    test service container

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
-use App\Http\Controllers\ComponentTestController;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\ComponentTestController;
+use App\Http\Controllers\LifeCycleTestController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -23,5 +23,6 @@ Route::get('/dashboard', function () {
 
 Route::get('/component-test1', [ComponentTestController::class, 'showComponent1']);
 Route::get('/component-test2', [ComponentTestController::class, 'showComponent2']);
+Route::get('/servicecontainertest', [LifeCycleTestController::class, 'showServiceContainer']);
 
 require __DIR__.'/auth.php';
